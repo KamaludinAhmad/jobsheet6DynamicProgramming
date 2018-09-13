cbc156e6fa0d98f662a193c3188da3a0825285a3
ASUS <ASUS@KAMAL>
2018-09-13 08:34:17 +0700

    Tambah Tampil Hasil

--- src/dynamicprogramming/HitungFibonacci.java
+++ src/dynamicprogramming/HitungFibonacci.java
@@ -5,6 +5,7 @@
  */
 package dynamicprogramming;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -27,6 +28,7 @@ public class HitungFibonacci {
         
         tampilJudul(identitas);
         int n = tampilInput();
+        BigInteger hasil = fibo(n);
     }
     private static int tampilInput(){
         Scanner scanner = new Scanner(System.in);
@@ -34,6 +36,17 @@ public class HitungFibonacci {
         System.out.print("Bilangan ke-: ");
         int n = scanner.nextInt();
         return n;
+    }
+    private static BigInteger fibo(int n){
+         BigInteger[] hasil = new BigInteger[n];
+         
+         hasil[0] = BigInteger.ONE;
+         hasil[1] = BigInteger.ONE;
+         
+         for (int i = 2; 1< n; i++){
+             hasil[1] = hasil[i-1].add(hasil[1-2]);
+         }
+         return hasil[n-1];
     }
 }
 
